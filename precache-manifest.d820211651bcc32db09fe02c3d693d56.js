52fd4f818541a5fdf44d11540078b4220f35d5bc
arczhang <arczhang@tencent.com>
2019-08-29 10:16:42 +0800

    up

---
+++ precache-manifest.d820211651bcc32db09fe02c3d693d56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde33393a4cac2f3d8dac7e1e5ef6ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8afcb072fab8a278419",
+    "url": "/static/css/2.2d7ed268.chunk.css"
+  },
+  {
+    "revision": "4a0e7d6f11a6f9725db2",
+    "url": "/static/css/main.6e2e0e21.chunk.css"
+  },
+  {
+    "revision": "b8afcb072fab8a278419",
+    "url": "/static/js/2.46a6389b.chunk.js"
+  },
+  {
+    "revision": "4a0e7d6f11a6f9725db2",
+    "url": "/static/js/main.7530fab2.chunk.js"
+  },
+  {
+    "revision": "bb9faa6dedc87ff36577",
+    "url": "/static/js/runtime~main.4a37795a.js"
+  },
+  {
+    "revision": "a5169ac840f79fa2260a540dc5164d3c",
+    "url": "/static/media/header.a5169ac8.jpg"
+  }
+]);
